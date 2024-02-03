777c2872de86cd3ef79fd7cbb246b513009523ed
leekyuho <rbgh0114@naver.com>
2024-02-04 04:45:21 +0900

    feat: utils 폴더 생성 및 type.ts 구현

---
+++ src/utils/type.ts
@@ -0,0 +1,9 @@
+export interface FilterConditions {
+  title: string | null;
+  isFree: string[];
+}
+//course information data type
+export type courseInfo = {
+  title: string;
+  short_description: string;
+};
