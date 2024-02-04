845d003fb36c3050d617cdb74f8a686dd73c8bc1
leekyuho <rbgh0114@naver.com>
2024-02-04 14:53:44 +0900

    feat: useDebounce 작성 및 useFilter 구현

---
+++ src/hooks/useFilter.ts
@@ -0,0 +1,31 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import useDebounce from './useDebounce';
+const DEFAULT_DELAY = 300;
+export const useFilter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [keyword, setKeyword] = useState<string>('');
+  const [price, setPrice] = useState<string[]>([]);
+  const searchDebounce = useDebounce((input: string) => {
+    if (input.trim() !== '') {
+      searchParams.set('keyword', input);
+    } else {
+      searchParams.delete('keyword');
+    }
+    setSearchParams(searchParams);
+  }, DEFAULT_DELAY);
+
+  const handleKeywordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+    searchDebounce(e.target.value);
+  };
+
+  return {
+    keyword,
+    price,
+    setPrice,
+    searchParams,
+    setSearchParams,
+    handleKeywordChange,
+  };
+};
