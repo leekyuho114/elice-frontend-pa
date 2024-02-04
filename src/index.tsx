14b64ccd790fbf959c0641b81ddccc06180be6c5
leekyuho <rbgh0114@naver.com>
2024-02-04 17:10:45 +0900

    feat: global style 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'styles/font.css';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from 'styles/GlobalStyle';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
